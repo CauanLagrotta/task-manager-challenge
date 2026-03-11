d04d85aa6d11032fed5a391df808add88f891cc0
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-03-11 16:32:09 -0300

    Initial commit

---
+++ src/main/java/com/cauanlagrotta/task_manager_challenge/TaskManagerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.cauanlagrotta.task_manager_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cauanlagrotta/task_manager_challenge/TaskManagerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauanlagrotta.task_manager_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
